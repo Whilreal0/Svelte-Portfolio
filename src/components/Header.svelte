1ce2138f463738f1bdbc80e65948b08de9e2ecd8
whilreal0 <whil.real@gmail.com>
2024-02-07 22:10:28 +0800

    fix

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,8 @@
 <script>
   import { onMount } from "svelte";
-  export let y;
+  export let y
   export let navItems;
+  export let navSocials;
   let isOpen = false;
 
   // for opening and closing nav
@@ -29,12 +30,18 @@
   class={"py-3 px-3 w-full flex md:hidden items-center transition ease-in duration-300 justify-between bg-[#DDD0C8]" +
     (y > 0 ? " sticky  top-0 left-0 right-0 drop-shadow-md" : "static ")}
 >
-  <span class="text-3xl ">WR</span>
-  <button class="md:hidden flex {isOpen ? 'hidden' : 'flex'}" on:click={navOpen}>
-    <iconify-icon class=" " icon="charm:menu-hamburger" height="30"  />
+  <span class="text-3xl">WR</span>
+  <button
+    class="md:hidden flex {isOpen ? 'hidden' : 'flex'}"
+    on:click={navOpen}
+  >
+    <iconify-icon class=" " icon="charm:menu-hamburger" height="30" />
   </button>
-  <button class="items-end md:hidden flex {isOpen ? 'flex' : 'hidden'}" on:click={navOpen}>
-    <iconify-icon class=" " icon="f7:xmark" height="30"  />
+  <button
+    class="items-end md:hidden flex {isOpen ? 'flex' : 'hidden'}"
+    on:click={navOpen}
+  >
+    <iconify-icon class=" " icon="f7:xmark" height="30" />
   </button>
 
   {#if isOpen}
@@ -48,15 +55,26 @@
     >
     </a>
     <!-- content here -->
-    <div class="bg-red-300 absolute md:hidden  gap-5 py-3 px-4  w-full right-0 top-[60px]">
+    <div
+      class="bg-[#DDD0C8] absolute md:hidden gap-5 py-3 px-4 w-full right-0 top-[60px] flex flex-col items-center"
+    >
       <!-- <div class="bg-yellow-200 w-full flex items-start justify-end">
         <button class="items-end md:hidden flex {isOpen ? 'flex' : 'hidden'}" on:click={navOpen}>
           <iconify-icon class=" " icon="f7:xmark" height="30"  />
         </button>
       </div> -->
-      <ul class="flex flex-col items-center">
+      <ul class="text-center">
         {#each navItems as item}
           <li><a href={item.link} on:click={closeMenu}>{item.name}</a></li>
+        {/each}
+      </ul>
+      <ul class="">
+        {#each navSocials as item}
+          <li>
+            <a href={item.link}>
+              <iconify-icon class=" " icon={item.name} height="30" />
+            </a>
+          </li>
         {/each}
       </ul>
     </div>
